f55989100de5c351c8974a24986d614b281177a0
zhang jilin <jlzhang@nltechdev.com>
2019-06-21 08:45:51 +0800

    Tag test

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -1,15 +1,12 @@
-import React from 'react';
-import _ from 'lodash';
-import { square } from '../math.js';
+import React from "react";
+import _ from "lodash";
+import { square } from "../math.js";
 
-class HelloWorld extends React.Component{
-    
-  render(){
-    let {num,styleNames} = this.props;
-    let text = _.join(['Hello World',`${num}: ${square(num)}`],' and ');
-    return (<div className={styleNames}>
-      {text}
-    </div>);
+class HelloWorld extends React.Component {
+  render() {
+    let { num, styleNames } = this.props;
+    let text = _.join(["Tag Test", `${num}: ${square(num)}`], " and ");
+    return <div className={styleNames}>{text}</div>;
   }
 }
 
--- test/HelloWorldTest.js
+++ test/HelloWorldTest.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { expect } from 'chai';
-import { shallow } from 'enzyme';
+import HelloWorld from "../src/components/HelloWorld";
+import React from "react";
+import { expect } from "chai";
+import { shallow } from "enzyme";
 
-import HelloWorld from '../src/components/HelloWorld';
-
-describe('Hello World',()=>{
-  it('render component',()=>{
-    let num = 10, names = 'hello';
+describe("Hello World", () => {
+  it("render component", () => {
+    let num = 10,
+      names = "hello";
     const wrapper = shallow(<HelloWorld num={num} styleNames={names} />);
 
     expect(wrapper.props().className).to.equal(names);
-    expect(wrapper.text()).to.equal(`Hello World and ${num}: ${num*num}`);
+    expect(wrapper.text()).to.equal(`Tag Test and ${num}: ${num * num}`);
   });
 });
